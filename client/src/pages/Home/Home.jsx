e4cd433b7f1394370f006aa30a73e4342bb6cf20
isin <jon.s124524@gmail.com>
2024-01-01 20:13:06 +0200

    add a basic function

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chat from '../../components/Chat/Chat'
+
+const Home = () => {
+  return (
+    <div className="w-full h-full">
+      <Chat />
+    </div>
+  )
+}
+
+export default Home
