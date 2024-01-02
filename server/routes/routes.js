00ab312a252b7fcb7e8ee175ce244cb38b3560a6
isin <jon.s124524@gmail.com>
2024-01-02 23:17:19 +0200

    add a basic function

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -33,7 +33,8 @@ router.get('/api/v1/auth', async (req, res) => {
     res.cookie('token', token, {
       maxAge: 1000 * 60 * 60 * 48,
       path: '/',
-      httpOnly: true
+      httpOnly: true,
+      sameSite: 'none'
     })
     res.json({ status: 'ok', token, user })
   } catch (err) {
@@ -51,7 +52,8 @@ router.post('/api/v1/login', async (req, res) => {
     res.cookie('token', token, {
       maxAge: 1000 * 60 * 60 * 48,
       path: '/',
-      httpOnly: true
+      httpOnly: true,
+      sameSite: 'none'
     })
     res.json({ status: 'ok', token, user })
   } catch (err) {
